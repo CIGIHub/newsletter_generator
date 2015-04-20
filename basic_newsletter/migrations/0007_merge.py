23188dd7a3dff08b37b3cb642f0f3034173b0193
Natasha Scott <namscott@gmail.com>
2015-04-20 15:42:10 -0400

    adding migration merge

---
+++ basic_newsletter/migrations/0007_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('basic_newsletter', '0006_auto_20150417_2037'),
+        ('basic_newsletter', '0006_auto_20150417_1712'),
+    ]
+
+    operations = [
+    ]
