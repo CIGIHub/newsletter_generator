8873ab396584867fbd0014d17c5ac58a2b173d56
Caroline Simpson <csimpson@cigionline.org>
2015-04-23 15:49:45 -0400

    Adding merge migration.

---
+++ basic_newsletter/migrations/0008_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('basic_newsletter', '0007_merge'),
+        ('basic_newsletter', '0007_auto_20150423_1934'),
+    ]
+
+    operations = [
+    ]
